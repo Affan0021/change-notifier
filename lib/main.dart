820c2325989b924819762bea17aa57416ffb7dd3
Affan454 <f190168@nu.edu.pk>
2021-10-11 11:24:36 -0700

    Multiproviders

--- lib/main.dart
+++ lib/main.dart
@@ -6,67 +6,105 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<MyModel>( //      <--- ChangeNotifierProvider
-      create: (context) => MyModel(),
+    return MultiProvider( //                                     <--- MultiProvider
+      providers: [
+        ChangeNotifierProvider<MyModel>(create: (context) => MyModel()),
+        ChangeNotifierProvider<AnotherModel>(create: (context) => AnotherModel()),
+      ],
       child: MaterialApp(
         home: Scaffold(
           appBar: AppBar(title: Text('My App')),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          body: Column(
             children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
 
-              Container(
-                  padding: const EdgeInsets.all(20),
-                  color: Colors.green[200],
-                  child: Consumer<MyModel>( //                  <--- Consumer
-                    builder: (context, myModel, child) {
-                      return ElevatedButton(
-                        child: Text('Do something'),
-                        onPressed: (){
-                          myModel.doSomething();
+                  Container(
+                      padding: const EdgeInsets.all(20),
+                      color: Colors.green[200],
+                      child: Consumer<MyModel>( //            <--- MyModel Consumer
+                        builder: (context, myModel, child) {
+                          return RaisedButton(
+                            child: Text('Do something'),
+                            onPressed: (){
+                              // We have access to the model.
+                              myModel.doSomething();
+                            },
+                          );
                         },
-                      );
-                    },
-                  )
+                      )
+                  ),
+
+                  Container(
+                    padding: const EdgeInsets.all(35),
+                    color: Colors.blue[200],
+                    child: Consumer<MyModel>( //              <--- MyModel Consumer
+                      builder: (context, myModel, child) {
+                        return Text(myModel.someValue);
+                      },
+                    ),
+                  ),
+
+                ],
               ),
 
-              Container(
-                padding: const EdgeInsets.all(35),
-                color: Colors.blue[200],
-                child: Consumer<MyModel>( //                    <--- Consumer
-                  builder: (context, myModel, child) {
-                    return
-                      Text(
-                        myModel.someValue,
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontFamily: 'OpenSans',
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+              // SizedBox(height: 5),
 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
 
+                  Container(
+                      padding: const EdgeInsets.all(20),
+                      color: Colors.red[200],
+                      child: Consumer<AnotherModel>( //      <--- AnotherModel Consumer
+                        builder: (context, myModel, child) {
+                          return RaisedButton(
+                            child: Text('Do something'),
+                            onPressed: (){
+                              myModel.doSomething();
+                            },
+                          );
+                        },
+                      )
+                  ),
 
-                    );
-                  },
-                ),
+                  Container(
+                    padding: const EdgeInsets.all(35),
+                    color: Colors.yellow[200],
+                    child: Consumer<AnotherModel>( //        <--- AnotherModel Consumer
+                      builder: (context, anotherModel, child) {
+                        return Text('${anotherModel.someValue}');
+                      },
+                    ),
+                  ),
+
+                ],
               ),
-
             ],
           ),
         ),
       ),
     );
+
   }
 }
 
-class MyModel with ChangeNotifier { //                          <--- MyModel
+class MyModel with ChangeNotifier { //                        <--- MyModel
   String someValue = 'Hello';
-
   void doSomething() {
     someValue = 'Goodbye';
     print(someValue);
     notifyListeners();
+  }
+}
 
+class AnotherModel with ChangeNotifier { //                   <--- AnotherModel
+  int someValue = 0;
+  void doSomething() {
+    someValue = 5;
+    print(someValue);
+    notifyListeners();
   }
 }
